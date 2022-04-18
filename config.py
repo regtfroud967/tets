ed84a3c07cc6588882f4a138f1b2d4d184f16554
regtfroud967 <103933774+regtfroud967@users.noreply.github.com>
2022-04-18 13:49:40 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+apihash = '786c3bde1b0d3556b408497b6ead4739'
+apiid = 7014088
+
+
---
+++ install.sh
@@ -0,0 +1,6 @@
+apt install python -y
+pip install requests
+pip install base64
+pip install telethon
+pip install beautifulsoup4
+pip install colorama
